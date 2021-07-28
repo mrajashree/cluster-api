d832a507efff089c40ecea099d9e8b15cd290661
Rajashree Mandaogane <mandaor@amazon.com>
2021-07-28 13:30:36 -0700

    Add kubebuilder rbac annotation for clusterRole change
    The ClusterRole for cluster controller and KCP controller has permissions
    on the etcdadm resources. These were added without using the kubebuilder
    annotation, causing them to get removed on running `make generate-manifests`
    This commit adds the kubebuilder rbac annotation for the required changes
    so as to retain them after running the manifests target
    
    cr: https://code.amazon.com/reviews/CR-54484976

--- controlplane/kubeadm/controllers/controller.go
+++ controlplane/kubeadm/controllers/controller.go
@@ -61,6 +61,7 @@ import (
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io;bootstrap.cluster.x-k8s.io;controlplane.cluster.x-k8s.io,resources=*,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
 // +kubebuilder:rbac:groups=cluster.x-k8s.io,resources=machines;machines/status,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=etcdcluster.cluster.x-k8s.io,resources=*,verbs=get;list;watch
 
 // KubeadmControlPlaneReconciler reconciles a KubeadmControlPlane object
 type KubeadmControlPlaneReconciler struct {
